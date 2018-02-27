4f64615cd8d115c979494c8187340c3f33dd9e9c
Fei <d0449763@fcu.edu.tw>
2018-02-27 11:15:19 +0800

    更新

--- 0227/BubbleSort.java
+++ 0227/BubbleSort.java
@@ -1,10 +1,3 @@
-What is the problem of this code?
-
-https://github.com/nlhsueh/sqa/blob/master/src/debug/BubbleSort.java
-
-
-------------------------------------------------------------------------------------
-
 import java.util.Random;
 
 public class BubbleSort {
@@ -13,24 +6,41 @@ public class BubbleSort {
 		int[] data;
 		data = new int[10];
 		Random r = new Random();
-
+		
+        int a [] = {45,2,36,89,40};
+        
 		for (int i = 0; i < 10; i++) {
 			data[i] = r.nextInt(100);
 		}
 		
 		//測試用顯示目前值
+		System.out.println("\n 隨機產生的數列");
 		for (int i = 0; i < 10; i++) {
-			System.out.println(data[i]);
+			System.out.print(data[i] + " ");
+		}
+		
+		//測資
+		System.out.println("\n 測資 : 45 2 36 89 10");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
 		}
 
 		BubbleSort bs = new BubbleSort();
 
 		bs.bubbleSort(data);
-		System.out.println("Bubble sort complete");
+		bs.bubbleSort(a);
+		System.out.println("\nBubble sort complete");
 		
 		//測試用顯示完成BubbleSort值
+		System.out.println("\n 隨機產生的數列經過氣泡排序");
 		for (int i = 0; i < 10; i++) {
-			System.out.println(data[i]);
+			System.out.print(data[i] + " ");
+		}
+		
+		//測資
+		System.out.println("\n測資 要長這樣:2 36 40 45 89");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
 		}
 
 	}
@@ -42,10 +52,9 @@ public class BubbleSort {
 		for (int path = 0; path < length; path++) {
 			for (int i = 0; i < length - path; i++) {
 				if (data[i] > data[i + 1]) {
-        //swap錯誤
-				  temp = data[i]; 
-          data[i] = data[i+1];
-          data[i+1] = temp;
+					temp = data[i]; 
+                    data[i] = data[i+1];
+                    data[i+1] = temp;
 				}
 			}
 		}
