fe6ab3b14d4101862b7a2f9bfa88e9b0c7f5aec0
Fei <d0449763@fcu.edu.tw>
2018-02-27 11:46:23 +0800

    Update BubbleSort.java
    Topic 1: introduction
    
    First Try: BubbleSort
    what is the problem of the Bubble sort?
    Defect
    the code has bug
    it does not show the sorting result (difficult to test; TESTABILITY is bad)
    Add a print(int d[]) to show the data content
    bad naming (length = d.length-1 !!!)
    bad naming (path???)
    Use refactor >> rename to rename the path
    exchanging the data is not easy to read
    Add a swap(int data[], int i, int j) to swap the data
    Test
    Do you have enough test cases?
    prepare the test cases
    [1,2,3,4], [4,3,2,1], [1,3,2,4], [1,1,1,1], [1], [], [1,2,3] etc.
    You know the results
    compare the expected and real_result?
    How?
    automaitc testing
    
    Debug perspective
    test the program using the debug perspective
    breakpoint and watch variable

--- 0227/BubbleSort.java
+++ 0227/BubbleSort.java
@@ -1,3 +1,4 @@
+
 import java.util.Random;
 
 public class BubbleSort {
@@ -6,68 +7,74 @@ public class BubbleSort {
 		int[] data;
 		data = new int[10];
 		Random r = new Random();
-		
-        int a [] = {45,2,36,89,40};
-        
+
+		int a[][] = { { 45, 2, 36, 89, 40 }, { 1, 1, 1, 1, 1, 1 }, { 1, 2, 3, 4, 5 }, { 321, 325, 5, 34, 52, 33, 45 },
+				{ 5, 4, 3, 2, 1 }, { 33, 2, 23, 5, 3, 2, 5, 32, 5 } };
+
 		for (int i = 0; i < 10; i++) {
 			data[i] = r.nextInt(100);
 		}
-		
-		//測試用顯示目前值
+
+		// 測試用顯示目前值
 		System.out.println("\n 隨機產生的數列");
 		for (int i = 0; i < 10; i++) {
 			System.out.print(data[i] + " ");
 		}
-		
-		//測資
-		System.out.println("\n 測資 : 45 2 36 89 10");
+
+		// 測資
+		System.out.println("\n 測資");
 		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+			System.out.println("");
 		}
 
 		BubbleSort bs = new BubbleSort();
 
 		bs.bubbleSort(data);
-		bs.bubbleSort(a);
+
+		for (int i = 0; i < a.length; i++) {
+			bs.bubbleSort(a[i]);
+		}
 		System.out.println("\nBubble sort complete");
-		
-		//測試用顯示完成BubbleSort值
+
+		// 測試用顯示完成BubbleSort值
 		System.out.println("\n 隨機產生的數列經過氣泡排序");
 		for (int i = 0; i < 10; i++) {
 			System.out.print(data[i] + " ");
 		}
-		
-		//測資
+
+		// 測資
 		System.out.println("\n測資 要長這樣:2 36 40 45 89");
 		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+			System.out.println("");
 		}
 
 	}
 
 	public void bubbleSort(int[] data) {
-		
-		
-		//變數名稱錯誤
+		// 變數名稱錯誤
 		int length = data.length;
-		int temp = 0;
-		
-		//這裡應該表示回合(使用Refactor->Rename的功能)
-		for (int round = 0; round < length-1; round++) {
-			for (int i = 0; i < length - round-1; i++) {
+
+		// 這裡應該表示回合(使用Refactor->Rename的功能)
+		for (int round = 0; round < length - 1; round++) {
+			for (int i = 0; i < length - round - 1; i++) {
 				if (data[i] > data[i + 1]) {
-					swap(data,i,i+1);
+					swap(data, i, i + 1);
 				}
 			}
 		}
 	}
-	//把swap抽出來
-	public static void swap(int[] data, int a, int b) {  
-	      int temp = data[a];  
-	      data[a] = data[b];  
-	      data[b] = temp;  
-	}  
-	
-	
-	
+
+	// 把swap抽出來
+	public static void swap(int[] data, int a, int b) {
+		int temp = data[a];
+		data[a] = data[b];
+		data[b] = temp;
+	}
+
 }
